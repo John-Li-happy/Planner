5ff12bf6d032b214a29406d70177626f66825d59
John-Li-happy <63318597+John-Li-happy@users.noreply.github.com>
2021-05-10 16:34:57 -0400

    more edition made

---
+++ PreviewWidget/PreviewWidget.swift
@@ -0,0 +1,25 @@
+//
+//  PreviewWidget.swift
+//  PreviewWidget
+//
+//  Created by Zhaoyang Li on 5/2/21.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct WidgetView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                Text("this is planner")
+                
+                Spacer()
+            }
+        }
+    }
+    
+        
+}
